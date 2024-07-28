79773d50cc3158bfa5792c3b7f3ad59e352b9bb5
aa13245 <wl031512@gmail.com>
2024-07-28 17:03:23 +0900

    0728

--- Assets/XR/JSW/Scripts/Inventory_JSW.cs
+++ Assets/XR/JSW/Scripts/Inventory_JSW.cs
@@ -5,69 +5,93 @@ using static UnityEditor.Progress;
 
 public class Inventory_JSW : MonoBehaviour
 {
-    // 주무기 구조체
-    public struct Slot
-    {
-        public ItemTable_JSW.Items item;
-        public int value1;
-        public int value2;
-        public Slot(ItemTable_JSW.Items item, int value1, int value2)
-        {
-            this.item = item;
-            this.value1 = value1;
-            this.value2 = value2;
-        }
-    }
+    // 시작 기본권총
+    public GameObject pistolPrefab;
+
     // 슬롯 4개
-    Slot[] slots = new Slot[4];
-    public Slot this[int idx] { get{ return slots[idx]; } }
+    Item_JSW[] slots = new Item_JSW[4];
+    public Item_JSW this[int idx] { get{ return slots[idx]; } }
 
     // Start is called before the first frame update
     void Start()
     {   // 슬롯 초기화
-        slots[0] = new Slot(ItemTable_JSW.Items.none, 0, 0);
-        slots[1] = new Slot(ItemTable_JSW.Items.pistol, 15, 0);
-        slots[2] = new Slot(ItemTable_JSW.Items.none, 0, 0);
-        slots[3] = new Slot(ItemTable_JSW.Items.none, 0, 0);
+        slots[0] = null;
+        if (pistolPrefab != null)
+        {
+            GameObject pistol = Instantiate(pistolPrefab);
+            pistol.SetActive(false);
+            slots[1] = pistol.GetComponent<Item_JSW>();
+        }
+        slots[2] = null;
+        slots[3] = null;
     }
 
     // 아이템 줍기
-    public bool PickUp(ItemTable_JSW.Items itemEnum, int value1 = 1, int value2 = 0)
-    {
+    public bool PickUp(GameObject itemObj)
+    {   // 아이템 레이어 체크
+        if (itemObj.layer != LayerMask.NameToLayer("Item_JSW")) return false;
+        // 아이템 컴포넌트 접근
+        Item_JSW itemComp = null;
+        Transform currTf = itemObj.transform;
+        while (currTf != null)
+        {
+            itemComp = currTf.GetComponent<Item_JSW>();
+            if (itemComp != null) break;
+            currTf = currTf.parent;
+        }
+        if (itemComp == null) return false;
+
         // 주무기 교체
-        object item = ItemTable_JSW.instance.itemTable[itemEnum];
+        int slot = 0;
+        object item = ItemTable_JSW.instance.itemTable[itemComp.kind];
         if (item is ItemTable_JSW.MainWeapon)
         {
-            slots[0] = new Slot(itemEnum, value1, value2);
-            return true;
+            slot = 0;
         }
         // 보조무기 교체
         else if (item is ItemTable_JSW.SubWeapon || item is ItemTable_JSW.MeleeWeapon)
         {
-            slots[1] = new Slot(itemEnum, value1, value2);
-            return true;
+            slot = 1;
         }
         // 투척류 교체
         else if (item is ItemTable_JSW.Projectile)
         {   // 현재 소지템과 동일한지 확인
-            if (slots[2].item != itemEnum)
+            if (slots[2].kind != itemComp.kind)
             {
-                slots[2] = new Slot(itemEnum, value1, value2);
-                return true;
+                slot = 2;
             }
             else return false;
         }
         // 회복템 교체
         else if (item is ItemTable_JSW.Recovery)
         {   // 현재 소지템과 동일한지 확인
-            if (slots[3].item != itemEnum)
+            if (slots[3].kind != itemComp.kind)
             {
-                slots[3] = new Slot(itemEnum, value1, value2);
-                return true;
+                slot = 3;
             }
             else return false;
         }
         else return false;
+        // 교체
+        if (slots[slot] != null)
+        {   // 기존 아이템 뱉기
+            slots[slot].transform.position = transform.position + transform.forward * 0.4f + Vector3.up * 1.6f;
+            slots[slot].gameObject.SetActive(true);
+        }
+        slots[slot] = itemObj.GetComponent<Item_JSW>();
+        itemObj.SetActive(false);
+        return true;
+    }
+    public bool Drop(int slot)
+    {
+        if (slots[slot] != null)
+        {
+            slots[slot].transform.position = transform.position + transform.forward * 0.4f + Vector3.up * 1.6f;
+            slots[slot].gameObject.SetActive(true);
+            slots[slot] = null;
+            return true;
+        }
+        return false;
     }
     // 발사 & 사용
     public bool Use(int slot)
@@ -76,7 +100,11 @@ public class Inventory_JSW : MonoBehaviour
         {
             slots[slot].value1--;
             // 무기 아닐 경우 다 사용하면 빈 손으로 변경
-            if (slots[slot].value1 == 0 && slot >= 2) slots[slot] = new Slot(ItemTable_JSW.Items.none, 0, 0);
+            if (slots[slot].value1 == 0 && slot >= 2)
+            {
+                Destroy(slots[slot]);
+                slots[slot] = null;
+            }
             return true;
         }
         else return false;
@@ -85,7 +113,7 @@ public class Inventory_JSW : MonoBehaviour
     {   // 주무기
         if (slot == 0)
         {
-            if (ItemTable_JSW.instance.itemTable[slots[0].item] is ItemTable_JSW.MainWeapon item)
+            if (ItemTable_JSW.instance.itemTable[slots[0].kind] is ItemTable_JSW.MainWeapon item)
             {   // 가득 찼거나 보유 총알이 없을 때
                 if (slots[0].value1 == item.magazineCapacity || slots[0].value2 == 0) return false;
                 else
@@ -104,7 +132,7 @@ public class Inventory_JSW : MonoBehaviour
         // 보조무기
         else if (slot == 1)
         {
-            if (ItemTable_JSW.instance.itemTable[slots[1].item] is ItemTable_JSW.SubWeapon item)
+            if (ItemTable_JSW.instance.itemTable[slots[1].kind] is ItemTable_JSW.SubWeapon item)
             {   
                 if (slots[1].value1 == item.magazineCapacity) return false;
                 else
---
+++ Assets/XR/JSW/Scripts/Item_JSW.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_JSW : MonoBehaviour
+{
+    public ItemTable_JSW.Items kind;
+    public int value1;
+    public int value2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
